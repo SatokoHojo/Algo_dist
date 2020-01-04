a541ff1a50bdc9e9ddb1c3c8e762353fb35134b5
Claire <skyswords@skyswords-Akoya-E7416>
2020-01-04 15:28:47 +0100

    No more errors but the algo is not correct

--- src/GeneralNode.java
+++ src/GeneralNode.java
@@ -6,13 +6,14 @@ import java.util.BitSet;
 import java.util.List;
 
 public class GeneralNode extends Node {
-    static int nb_nodes; // assuming for now we calculate it somehow before starting
-    static int delta; // same
+    static int nb_nodes = 10; // assuming for now we calculate it somehow before starting
+    static int delta = 2; // same
     private ArrayList<Node> fathers;
     private int[] colors;
     private int[] father_colors;
     private int l = -1;
     private int l_prime;
+    private boolean init = true;
     private int to_remove = 5;
     private boolean shift = true;
     private int final_color = -1;
@@ -79,7 +80,7 @@ public class GeneralNode extends Node {
         for (Message m : l) {
             max = Math.max(max, (int) m.getContent());
         }
-        boolean[] present = new boolean[max];
+        boolean[] present = new boolean[max+1];
         for (Message m: l) {
             present[(int)m.getContent()] = true;
         }
@@ -101,18 +102,6 @@ public class GeneralNode extends Node {
         father_colors = new int[delta];
 
         fathers = new ArrayList<Node>(delta);
-        List<Node> nei = getNeighbors();
-        int currently_free = 0;
-        for (Node n : nei) {
-            if (n.getID() < getID()) {
-                fathers.set(currently_free, n);
-                currently_free++;
-            }
-        }
-        for (int i = currently_free; i < delta; i++) {
-            fathers.set(i, this);
-            father_colors[i] = Math.max(1-getID(), 0);
-        }
 
         l = (int) Math.ceil(log2(nb_nodes));
         l_prime = l+1; //arbitrarily fixed so that l != l_prime
@@ -121,6 +110,21 @@ public class GeneralNode extends Node {
     @Override
     public void onClock() {
         // JBotSim executes this method on each node in each round
+        if (init) { // because the list of neighbours isn't fixed in onStart
+            List<Node> nei = getNeighbors();
+            int current_nb = 0;
+            for (Node n : nei) {
+                if (n.getID() < getID()) {
+                    fathers.add(n);
+                    current_nb++;
+                }
+            }
+            for (int i = current_nb; i < delta; i++) {
+                fathers.add(this);
+                father_colors[i] = Math.max(1-getID(), 0);
+            }
+            init = false;
+        }
         List<Message> messages = getMailbox();
         if (to_remove >= 3 && !reduction_started) {
             if (!messages.isEmpty()) {
